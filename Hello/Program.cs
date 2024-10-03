0198c3e2a6ddd14f50e22027894b1f110db6e595
watanabe-akihiro <k022g0118@g.neec.ac.jp>
2024-10-03 16:22:33 +0900

    プロジェクト ファイルを追加します。

---
+++ Hello/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# World");
+
+            //入力を待つ(一時停止の役割)
+            Console.ReadLine();
+        }
+    }
+}
