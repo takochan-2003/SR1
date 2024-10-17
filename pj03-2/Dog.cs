a76b287c2fff3c90fe13d54190d9e1d39d51962e
watanabe-akihiro <k022g0118@g.neec.ac.jp>
2024-10-17 16:16:42 +0900

    完成

---
+++ pj03-2/Dog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pj03_2
+{
+    internal class Dog
+    {
+        //状態や属性「データ」・・・フィールド
+        private bool hungryState;
+        //ふるまい「操作」・・・メソッド
+        //コンストラクタ(クラス名 Dog と伊那路名前のメソッド)
+        public Dog()
+        {
+            hungryState = true;//最初はお腹がすいている
+        }
+        public void Eat()//食べる
+        {
+            Console.WriteLine("ぱくぱく");
+            hungryState = false; //お腹はすいていない
+        }
+
+        public void Run()//走る
+        {
+            Console.WriteLine("わんわんわん");
+            hungryState = true;//お腹すいた
+        }
+
+        public bool IsHungry()//お腹すいてるか調べる
+        {
+            return hungryState;
+        }
+    }
+}
---
+++ pj03-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pj03_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog pochi = new Dog();
+
+            //最初の空腹状態を表示してみる
+            Console.WriteLine("空腹状態：{0}", pochi.IsHungry());
+
+            //pochiにエサをあげる
+            pochi.Eat();
+            Console.WriteLine("空腹状態：{0}", pochi.IsHungry());
+
+            //pochiを走らせる
+            pochi.Run();
+            Console.WriteLine("空腹状態：{0}", pochi.IsHungry());
+
+            //一時停止
+            Console.ReadLine();
+        }
+    }
+}
