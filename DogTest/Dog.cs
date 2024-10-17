bf50b954c25f4d6e5968b8ca2fc634e0ea1b3e15
watanabe-akihiro <k022g0118@g.neec.ac.jp>
2024-10-17 14:38:46 +0900

    0_2完了

---
+++ DogTest/Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogTest
+{
+    internal class Dog
+    {
+        public void Bark()
+        {
+            Console.WriteLine("ガウガウ");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("わんわんわん");
+        }
+    }
+}
---
+++ DogTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dogクラスのインスタンスhachiを生成する
+            Dog hachi = new Dog();
+            hachi.Bark();
+
+            //Dogクラスのインスタンスpochiを生成する
+            Dog pochi = new Dog();
+            pochi.Bark();
+            pochi.Run();
+
+            //一時停止
+            Console.ReadLine();
+        }
+    }
+}
